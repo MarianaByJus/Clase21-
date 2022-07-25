9f8d4044f28d5f025efdf0826fb3de6a268c0147
MarianaByJus <86327356+MarianaByJus@users.noreply.github.com>
2022-07-25 18:03:43 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var towerImg, tower;
+var doorImg, door, doorsGroup;
+var climberImg, climber, climbersGroup;
+var ghost, ghostImg;
+var invisibleBlockGroup, invisibleBlock;
+var gameState = "play"
+
+function preload(){
+  towerImg = loadImage("tower.png");
+  doorImg = loadImage("door.png");
+  climberImg = loadImage("climber.png");
+  ghostImg = loadImage("ghost-standing.png");
+  spookySound = loadSound("spooky.wav");
+}
+
+function setup(){
+  createCanvas(600,600);
+ 
+}
+
+function draw(){
+  background(0);
+  
+    
+    drawSprites();
+  }
+  
+  
+
+
